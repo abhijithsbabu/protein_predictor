17e6399dd99dff70bf924c4c57639aca27c916f9
ABHIJITHSBABU <abhijithsreevilas@gmail.com>
2023-09-22 19:31:07 +0530

    updated-length-printing

--- testfile.py
+++ testfile.py
@@ -3,13 +3,14 @@ from collections.abc import Iterable
 
 def parser(d_st, lvl, inx):
     tp = type(d_st)
-    print("Type = {}, index = {},level={}".format(tp,inx,lvl))
+    print("Type = {}, index = {},level={}".format(tp,inx,lvl), end = " ")
     if isinstance(d_st, Iterable):
+        print("size = ",len(d_st))
         i = 1
         for ele in d_st:
             parser(ele, lvl + 1,i)
             i+=1
-            if i>10:
+            if i>3:
                 print("Terminating level {} at current index".format(lvl+1))
                 break
 
